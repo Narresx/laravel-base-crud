d727ae6e54b3b64533ead5db04951ef1a813215e
Narresx <92748159+Narresx@users.noreply.github.com>
2022-03-22 15:38:21 +0100

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css">
+
+    <title>Comics 2.0</title>
+</head>
+
+<body>
+    <div>hello world</div>
+    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+</body>
+
+</html>
